150220f5c4b4f40aee5cf5a65fc3dc41a18ba925
Infihnity3 <zackchow0306@gmail.com>
2022-01-05 03:35:45 +0800

    added a js file for fundamentals part 3

---
+++ Fundamentals Part 3/index.js
@@ -0,0 +1,20 @@
+// Write a function called add7 that takes one number and returns that number + 7.
+function add7(a){
+    let b = a + 7;
+    return b;
+}
+// Write a function called multiply that takes 2 numbers and returns their product.
+function multiply(a, b){
+    let c = a * b;
+    return c;
+}
+
+// Write a function called capitalize that takes a string and returns that string with only the first letter capitalized. Make sure that it can take strings that are lowercase, UPPERCASE or BoTh.
+function capitalize(a){
+    return a.slice[0];
+}
+// Write a function called lastLetter that takes a string and returns the very last letter of that string:
+function lastLetter(a){
+    return a.slice[-1];
+}
+// lastLetter("abcd") should return "d"
